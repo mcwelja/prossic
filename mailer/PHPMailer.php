29af776933fb731064a6de5e266fe7ad9543752e
MladenVe <mcwelja@hotmail.com>
2019-12-28 21:03:17 +0100

    Update PHPMailer.php

--- mailer/PHPMailer.php
+++ mailer/PHPMailer.php
@@ -69,12 +69,12 @@ if (isset($_POST['cart'])) {
 $mail = new PHPMailer;
 
 //Server settings
-// $mail->SMTPDebug = SMTP::DEBUG_SERVER;                   // Enable verbose debug output
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
 $mail->isSMTP();                                            // Send using SMTP
 $mail->Host = 'smtp.gmail.com';                             // Set the SMTP server to send through
 $mail->SMTPAuth = true;                                     // Enable SMTP authentication
 $mail->Username = 'mcwelja@gmail.com';                      // SMTP username
-$mail->Password = 'MladenVeljkovic1990';                     // SMTP password
+$mail->Password = 'MladenVeljkovic1990';                    // SMTP password
 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
 $mail->Port = 587;                                          // TCP port to connect to
 
