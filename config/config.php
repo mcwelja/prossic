dae45021b12cda308b6a1396fefbc6cc44c5d460
MladenVe <mcwelja@hotmail.com>
2019-12-28 20:50:38 +0100

    Database access

--- config/config.php
+++ config/config.php
@@ -1,18 +1,26 @@
 <?php
 
 // ovde idu podaci za pristup bazi
-$servername = "localhost";
-$username = "root";
-$password = "";
 
-try {
-    $db = new PDO("mysql:host=$servername;dbname=prossic", $username, $password);
-    // set the PDO error mode to exception
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Connected successfully";
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+if ('127.0.0.1' ==$_SERVER['SERVER_ADDR']) {
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+
+} else {
+	$servername = "localhost";
+	$username = "root";
+	$password = "Prossic123";
 }
+
+	try {
+	    $db = new PDO("mysql:host=$servername;dbname=prossic", $username, $password);
+	    // set the PDO error mode to exception
+	    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	    // echo "Connected successfully";
+	} catch (PDOException $e) {
+	    echo "Connection failed: " . $e->getMessage();
+	}
 
 
 session_start();
