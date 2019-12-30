3d658e7dd25b462077cfa2da180a00bdbb9271b9
MladenVe <mcwelja@hotmail.com>
2019-12-30 20:07:50 +0100

    Update PHPMailer.php

--- mailer/PHPMailer.php
+++ mailer/PHPMailer.php
@@ -61,7 +61,7 @@ if (isset($_POST['cart'])) {
 $mail = new PHPMailer(true);
 
 //Server settings
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
 $mail->isSMTP();                                            // Send using SMTP
 $mail->Host = 'smtp.gmail.com';                             // Set the SMTP server to send through
 $mail->SMTPAuth = true;                                     // Enable SMTP authentication
